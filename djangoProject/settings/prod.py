648b52d1b802768ee6e407f3072ecf37bfa5aff4
julia-heo <100216331+julia-heo@users.noreply.github.com>
2024-04-30 08:19:24 +0900

    init

---
+++ djangoProject/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *  # noqa
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DEBUG = False
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR , "db.sqlite3"),
+    }
+}
